ef642fc3db81805d2391d268c92c01a0ea2d0d71
QuietSwami <fran.abm94@gmail.com>
2016-04-19 10:41:57 +0100

    fixed repository

--- data/options.js
+++ data/options.js
@@ -6,11 +6,7 @@ self.port.on("show", function(array){
 	rules = JSON.parse(array[1]);
 	blacklist.forEach(function(url){
 		if (url[url.length - 1] != "*"){
-<<<<<<< HEAD
-			$('<div class="alert alert-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close" id="'+url+'"><span aria-hidden="true">&times;</span></button>'+ url +'</div>').insertBefore('#add');
-=======
 			$('#sites').append('<div id="site" class="alert alert-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close" id="'+url+'"><span aria-hidden="true">&times;</span></button>'+ url +'</div>');
->>>>>>> 2733a999fe5ecae6aceabaad8c2e61a6a955e725
 		}
 	});
 	$("#rules").append('<div id="rule" class="alert alert-success alert-dismissible" role="alert"><button type="button" id="rule" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Rule #1</strong> Starting Time: ' + rules.starting_time + '; Ending Time: '+ rules.ending_time+'</div>');
@@ -34,7 +30,6 @@ self.port.on("show", function(array){
 						temp.push(url);
 					}
 				}
-
 			});
 			blacklist = temp;
 			self.port.emit("blacklist_change", blacklist);*/
@@ -47,12 +42,7 @@ self.port.on("show", function(array){
 			
 		}
 	});
-	$("#block").click(function() {
-		console.log("yo");
-		var site = $("#site").val();
-		console.log(site);
-		// self.port.emit("website-block", )
-	});
+
 });
 
 
